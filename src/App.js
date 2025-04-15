8260caad8e7b1438148dcb0eeaf77ac911a9016d
Manish Sabbani <manishsabbani1996@gmail.com>
2025-04-15 08:26:04 +0530

    create moviecast and story website and repository

---
+++ src/App.js
@@ -0,0 +1,150 @@
+import React from 'react'
+import ReactLogo from './Assets/SankranthikiVasthunam123.avif'
+import ReactLogo1 from './Assets/images (9).jpeg'
+import ReactLogo2 from './Assets/images (12).jpeg'
+import ReactLogo3 from './Assets/Aparichitudu.jpg'
+import './index.css';
+function App() {
+  return (
+    <div className='main_header'>
+      <div className='header'>
+        <img id="image_div" src={ReactLogo} alt='movie_album'></img>
+      </div>
+      <div className='main_list'>
+        <div className='unlist'>
+          <ul>
+            <li id="first"><b>Cast</b></li>
+            <li><a href="www.venkatesh.com"> Venkatesh</a> as ex-<a href="www.DCP">DCP</a> Yadagiri
+              Damodara Sinaraju "Raju"
+              alias YD Raju</li>
+            <li><a href="www.Aishwarya rajesh">Aishwarya Rajesh</a> as Bhagyalakshmi Sinaraju, Raju's wife</li>
+            <li><a href="Meenakshi Chaudhary">Meenakshi Chaudhary</a> as<a href="www.ACP.com"> ACP</a> C. Meenakshi
+              "Meenu", Raju's ex-girlfriend</li>
+            <li><a href="www.Naresh.com">Naresh</a> as <a href="www.TelanganaChiefMinister">Telangana Chief
+              Minister</a> D. Kesava</li>
+            <li><a href="www.P.Sai kumar">P. Sai kumar</a> Kumar as <a href="Constable">Constable</a> P.Manikya Rao
+            </li>
+            <li><a href="VTV Ganesh">VTV Ganesh</a> Ganesh as T.V. Ganesh, Party President</li>
+            <li><a href="SavadamanD.Banerjee">Sarvadaman D. Banerjee</a> as <a href="Schoolmaster">Schoolmaster</a>
+              Yadagiri</li>
+            <li><a href="UpendraLimaye">Upendra Limaye</a> as <a href="Jailer">Jailer</a> George Antony</li>
+            <li>Bheems Ceciroleo in an cameo appearnace as DJ in "Blockbuster Pongal"</li>
+            <li><a href="AnilRavipudi">Anil Ravipudi</a> as Raju's neighbour in a cameo appearnace</li>
+          </ul>
+        </div>
+      </div>
+      <div className='maindiv_para'>
+        <div className='div_para'>
+          <p id='story'><b>Story</b></p><br />
+          <p id="para" className='s_conclusion'> &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; Satya Akkela, a renowned businessman comes to India for a
+            businessman vacation to Telangana. Upon
+            the
+            party president's suggestion, he comes to eat in a farmhouse but gets kidnapped by an unknown gang.
+            Meenakshi, a police officer investigates, and takes help from her ex, a cop .</p>
+        </div>
+      </div><br />
+      <hr />
+      <div className='header'>
+        <img id="image_div" src={ReactLogo1} alt='movie_album'></img>
+      </div>
+      <div className='main_list'>
+        <div className='unlist'>
+          <ul>
+            <li id="first"><b>Cast</b></li>
+            <li><a href="www.AllariNaresh.com">Allari Naresh</a> as Relangi Rajababu as a comedian character</li>
+            <li><a href="www.GeethaSingh.com">Geetha Singh</a> as Soundarya</li>
+            <li><a href="Madhushalini.com">Madhu Shalini</a>as Rambha</li>
+            <li><a href="www.GiriBabu.com">Giri Babu </a>as Rajababu's father</li>
+            <li><a href="www.JayaPrakashReddy.com">Jaya Prakash Reddy</a> as S. V. Ranga Rao, Soundrya's father</li>
+            <li><a href="www.M.S.Narayana.com">M. S. Narayana</a>as Nagarjuna / Nag, Soundarya's grandfather</li>
+            <li><a href="www.DharmavarapuSubramanyam.com">Dharmavarapu Subramanyam</a>as SP Balasubramanyam</li>
+            <li><a href="www.Lakshmipati.com">Lakshmipati</a> as Nana Patekar / Aryan Rajesh, Car driver</li>
+            <li><a href="www.AVS.com">AVS</a>AVS as chidatala Appa Rao, Soundarya's neighbour</li>
+            <li><a href="www.L.B. Sriram">L. B. Sriram</a>as Purohitudu Veturi Sitarama Sastry</li>
+            <li><a href="AllariSubhashini.com">Allari Subhashini</a> as Bipasa Basu, chidatala Appa Rao's wife</li>
+          </ul>
+        </div>
+      </div>
+      <div className='maindiv_para'>
+        <div className='div_para'>
+          <p id="story"><b>Story</b></p>
+          <p id="para">&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; Relangi Rajababu is such a "nice guy" that he files a cas
+            against
+            a girl who attempted to
+            harass him, and did not succumb to her, because he wished to remain "pure" for the woman he marries.
+            After landing a job as an SI, his parents force him to get married because his dowry will be useful for
+            his sister's wedding. No girl wants to marry him now, because they wonder if he is man enough since he
+            rejected a girl like her. His attempts to make her grant him a divorce and whether he turns over a new
+            leaf is the rest of the story.</p>
+        </div>
+      </div><br />
+      <hr />
+      <div className='header'>
+        <img id="image_div" src={ReactLogo2} alt='movie_album'></img>
+      </div>
+      <div className='main_list'>
+        <div className='unlist'>
+          <ul>
+            <li id="first"><b>Cast</b></li>
+            <li><a href="www.Chiranjeevi.com">Chiranjeevi</a>Chiranjeevi is a Anji</li>
+            <li><a href="www.NamrataShirodkar.com">Namrata Shirodkar</a>as Swapna</li>
+            <li><a href="www.TinnuAnand.com">Tinnu Anand</a>as Veerendra Bhatia </li>
+            <li><a href="www.NagendraBabu.com">Nagendra Babu</a>as Sivanna</li>
+            <li><a href="www.NityaShetty.com">Nitya shetty</a> as Bby Nithya</li>
+            <li><a href="www.Ramireddy.com">Rami Reddy</a>as the local hunter</li>
+            <li><a href="www.RamyaKrishan.com">Ramya Krishna</a> has a special appearnace in the song "Chikbuk Pori"
+            </li>
+            <li><a href="www.ReemaSen.com">Reema Sen</a>has a special appearnace in the song "Mirapakaya Bajji"</li>
+            <li><a href="www.Alphonsa.com">Alphonsa</a> also has a special appearnace in the song "Chikbuk Pori"
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='maindiv_para'>
+        <div className='div_para'>
+          <p id="story"><b>Story</b></p>
+          <p id="para">&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; The Atmalingam, a symbol of Lord Shiva, possesses enormous
+            divine
+            powers. Once every 72 years,
+            the Akasha Ganga (Heavenly dimension of the Ganga river from the universe) flows onto the Atmalingam.
+            Those who drink Akasha Ganga's holy water become immortal and gain supernatural powers.</p>
+        </div>
+      </div><br />
+      <hr />
+      <div className='header'>
+        <img id="image_div" src={ReactLogo3} alt='movie_album'></img>
+      </div>
+      <div className='main_list'>
+        <div className='unlist'>
+          <ul>
+            <li id="first"><b>Cast</b></li>
+            <li><a href="www.SureshHeblikar.com">Suresh Heblikar</a>as a Prakash</li>
+            <li><a href="www.Shobha.com">Shobha</a>as Kusuma</li>
+            <li><a href="www.VasudevaRao.com">Vasudeva Rao</a>as Rangaiah</li>
+            <li><a href="www.Mohan.com">Mohan</a>as Shamu</li>
+            <li><a href="www.Vikram.com">Vikram</a>as Ramanujam</li>
+            <li><a href="www.Sadha.com">Sadha</a>as Nandini</li>
+            <li><a href="www.S.Shankar.com">S. Shankar</a>as director</li>
+            <li><a href="www.ReemaSen.com">Reema Sen</a>has a special appearance in the song "Mirapakaya Bajji"</li>
+            <li><a href="www.Alphonsa.com">Alphonsa</a> also has a special appearance in the song "Chikbuk Pori"
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className='maindiv_para'>
+        <div className='div_para'>
+          <p id="story"><b>Story</b></p>
+          <p id="para">&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; Ramanujam "Ambi" Iyengar, a straighforward consumer
+            protection
+            advocate from Thiruvallikeni,
+            Chennai, expects everyone to follow the law and prosecutes those who violate it, but his efforts are
+            unsuccessful due to circumstantial evidence favours the accused. His efforts to raise civic
+            awareness go
+            unacknowledged due to general lack of seriousness and widespread corruption. </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
